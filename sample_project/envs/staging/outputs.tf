41d014686c3c1e3e0e359e7dd8b7f8014f831764
n.f <n.fujita74@gmail.com>
2025-02-04 05:25:46 +0900

    adding staging terraform sample. and bug fix cfn template.

---
+++ sample_project/envs/staging/outputs.tf
@@ -0,0 +1,21 @@
+
+// for debug
+output "vpc-3az-vpc" {
+  value = module.vpc-3az.vpc
+}
+
+output "vpc-3az-publicsub1" {
+  value = module.vpc-3az.publicsub1
+}
+
+output "vpc-3az-publicsub2" {
+  value = module.vpc-3az.publicsub2
+}
+
+output "vpc-3az-publicsub3" {
+  value = module.vpc-3az.publicsub3
+}
+
+output "vpc-3az-private3-rtb" {
+  value = module.vpc-3az.private3-rtb
+}
